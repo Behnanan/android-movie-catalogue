24321f33f7ba9e8acf6353aa5154f1afab52d4db
Benjamin Hanna <benjaminhanna@benjamins-mbp.attlocal.net>
2021-07-06 09:54:51 -0500

    removed duplicate files

--- MovieCatalogue/app/src/main/java/com/example/navigationapplication/model/Movie.kt
+++
@@ -1,9 +0,0 @@
-package com.example.navigationapplication.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Movie(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResourceId: Int) {
-}
