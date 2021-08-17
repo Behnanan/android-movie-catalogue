17eee653baaff07db628389f8ceb8905ae6dd9ad
Benjamin Hanna <benjamin.hanna@medable.com>
2021-08-16 21:43:59 -0500

    refactored splash screen

--- app/src/main/java/com/example/moviecatalogue/MainActivity.kt
+++ app/src/main/java/com/example/moviecatalogue/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.moviecatalogue
 
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -18,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_MovieCatalogue)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -31,39 +28,16 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNavigationView.setupWithNavController(navController)
 
+        // this should be changed to a singleton
         // Setup the ActionBar with navController and 3 top level destinations
         appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.splashFragment, R.id.homeScreenFragment)
+            setOf(R.id.homeScreenFragment)
         )
-//        appBarConfiguration = AppBarConfiguration(
-//            setOf(R.id.titleScreen, R.id.leaderboard,  R.id.register)
-//        )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration)
     }
-
-//        val navHostFragment = supportFragmentManager
-//            .findFragmentById(R.id.navHostFragment) as NavHostFragment
-//
-//        val navController = navHostFragment.navController
-//
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//
-//    }
-//
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.navHostFragment)
-//        return navController.navigateUp(appBarConfiguration)
-//                || super.onSupportNavigateUp()
-//    }
-
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return item.onNavDestinationSelected(findNavController(R.id.navHostFragment))
-//                || super.onOptionsItemSelected(item)
-//    }
 }
